cf9f719a95ec296ed36ae8fb54cfe5086218a5d7
m98cc4m6bg-sketch <m98cc4m6bg@privaterelay.appleid.com>
2026-09-12 15:52:57 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    Doel: de app start ook zonder internet, maar een gepubliceerde
    update wint altijd zodra er wél internet is.
    Data gaat via cloud.js naar Supabase en wordt nooit gecachet. */
-const VERSIE = 'v29';
+const VERSIE = 'v30';
 const CACHE = 'glasopname-' + VERSIE;
 const SHELL = [
   './',
